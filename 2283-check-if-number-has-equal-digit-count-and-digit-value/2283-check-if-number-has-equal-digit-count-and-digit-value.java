a94cfc2446f0f8ebfb9e032fa9de42d4bf91fa81
Bala Subramanian V <vadivelbalu15@gmail.com>
2026-07-25 20:47:26 +0530

    Time: 3 ms (5.72%), Space: 43.7 MB (6.49%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
@@ -1,33 +1,16 @@
 class Solution {
     public boolean digitCount(String num) {
-        long num1 = Long.valueOf(num);
-        long num2 = num1;
-        Map<Integer, Integer> nee = new HashMap<>();
-        for (int i = 0; i < num.length(); i++) {
-            String s = num.charAt(i) + "";
-            int tem = Integer.valueOf(s);
-            nee.put(i, tem);
-        }
-        boolean ans = true;
-        System.out.println(nee);
-        for (int i1 = 0; i1 < num.length(); i1++) {
-            int times = nee.get(i1);
-            int check = 0;
-            for (int i = 0; i < num.length(); i++) {
-                String s = num.charAt(i) + "";
-                int tem = Integer.valueOf(s);
-                if (tem == i1) {
-                    check++;
-                }
-            }
-            if (times == check) {
-                check = 0;
-                continue;
-            } else {
-                ans = false;
-                break;
-            }
-        }
-        return ans;
+         int arr[]=new int[10];
+         for(int i=0;i<num.length();i++){
+             arr[num.charAt(i)-'0']+=1;
+         } 
+         String temp="";
+        for(int i=0;i<num.length();i++){
+            temp+= arr[i];
+         }
+         if(num.equals(temp)){
+            return true;
+         }
+         return false;
     }
 }
