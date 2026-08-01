95e8388e9d2b58f837ca33182319c5976cd078cf
Bala Subramanian V <vadivelbalu15@gmail.com>
2026-08-01 21:36:18 +0530

    Time: 0 ms (100%), Space: 42.6 MB (89.58%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] memo = new int[n][n];   
+        return maxDiff(nums, 0, n - 1, memo) >= 0;
+    }
+    private int maxDiff(int[] nums, int i, int j, int[][] memo) {
+        if (i == j) {
+            return nums[i];
+        }
+        if (memo[i][j] != 0) {
+            return memo[i][j];
+        }
+        int pickLeft = nums[i] - maxDiff(nums, i + 1, j, memo);
+        int pickRight = nums[j] - maxDiff(nums, i, j - 1, memo);
+        return memo[i][j] = Math.max(pickLeft, pickRight);
+    }
+}
