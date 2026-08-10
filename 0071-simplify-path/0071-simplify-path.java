6431ee8323fad4fec9b4a1e259ea71af3179e9fa
Balasubramanian V <vadivelbalu15@gmail.com>
2026-08-10 20:09:02 +0530

    Time: 8 ms (10.12%), Space: 44.8 MB (63.99%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+        String[] components = path.split("/+");      
+        for (String dir : components) {
+            if (dir.isEmpty() || dir.equals(".")) {
+                continue;
+            }
+            if (dir.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.removeLast();
+                }
+            } else {
+                stack.addLast(dir);
+            }
+        }     
+        StringBuilder result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/").append(dir);
+        }       
+        return result.length() == 0 ? "/" : result.toString();
+    }
+}
